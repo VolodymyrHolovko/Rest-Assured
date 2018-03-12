f0fcdc9ca728105f9372a8b42d95a7972af33d68
Volodymyr Holovko <volodymyr.holovko@magnise.com>
2018-03-12 17:16:38 +0200

    to__develop

--- src/main/java/Nomenclature/NomenclatureTests.java
+++ src/main/java/Nomenclature/NomenclatureTests.java
@@ -1,10 +1,9 @@
 package Nomenclature;
 
-import Models.Token.GetToken;
+import token.GetToken;
 import com.google.gson.Gson;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ResponseBody;
-import org.json.simple.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,6 +22,7 @@ public class NomenclatureTests {
     public void getToken() {
         GetToken getToken = new GetToken();
         this.token=getToken.GetToken();
+        System.out.println("evevevevefbdefbedbesbesbebebrerbevevevev");
     }
 
     @Test
--- src/main/java/token/GetToken.java
+++ src/main/java/token/GetToken.java
@@ -1,4 +1,4 @@
-package Frame.tests;
+package token;
 
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ResponseBody;
@@ -7,8 +7,6 @@ import org.json.simple.JSONObject;
 import static com.jayway.restassured.RestAssured.given;
 
 public class GetToken {
-    private final String baseURL = "http://cluster.test.eservia.com/api/v0.0";
-
 
     public String GetToken() {
         JSONObject requestParams = new JSONObject();
@@ -16,9 +14,9 @@ public class GetToken {
         requestParams.put("password", "manager");
         requestParams.put("deviceId", "string");
         requestParams.put("authTypeId", 1);
-        ResponseBody response = given().contentType(ContentType.JSON).body(requestParams).when().post(baseURL + "/Authorize").thenReturn().body();
+        ResponseBody response = given().contentType(ContentType.JSON).body(requestParams).when().post("http://cluster.test.eservia.com/api/v0.0/Authorize").thenReturn().body();
         String token = response.jsonPath().get("data.token");
-        token = "Bearer " + token;
-        return token;
+        String token1 = "Bearer " + token;
+        return token1;
     }
 }
