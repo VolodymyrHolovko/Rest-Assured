b09f525699030f7247f98b0cb193dbf941bf36f8
Volodymyr Holovko <volodymyr.holovko@magnise.com>
2018-05-18 16:55:07 +0300

    customer token

--- src/main/java/Auth/Users/GetUserToken.java
+++ src/main/java/Auth/Users/GetUserToken.java
@@ -1,6 +1,8 @@
 package Auth.Users;
 
 import com.google.gson.Gson;
+import com.jayway.restassured.filter.log.RequestLoggingFilter;
+import com.jayway.restassured.filter.log.ResponseLoggingFilter;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ResponseBody;
 import org.testng.annotations.Test;
@@ -9,7 +11,7 @@ import static com.jayway.restassured.RestAssured.given;
 
 public class GetUserToken {
     AuthUserData authUserData = new AuthUserData();
-    String token;
+    public String token;
     String baseURL = "https://auth.staging.eservia.com/api/v0.0/Auth/SignIn";
 
     @Test
@@ -17,6 +19,8 @@ public class GetUserToken {
         ResponseBody responseBody = given().
                 contentType(ContentType.JSON)
                 .body(authUserData.signIn())
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
                 .when().post(baseURL).thenReturn().body();
         AuthUserResponse authUserResponse = new Gson().fromJson(responseBody.asString(), AuthUserResponse.class);
         AuthUser authUser = authUserResponse.data;
