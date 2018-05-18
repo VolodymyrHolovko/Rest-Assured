bca9a5467c98678ce942e06452219ef7e54ddc0b
Max <lutkovec@gmail.com>
2018-05-18 16:57:01 +0300

    sdf

--- src/main/java/Orders/Order/OrderItemStatusData.java
+++ src/main/java/Orders/Order/OrderItemStatusData.java
@@ -1,12 +1,11 @@
 package Orders.Order;
 
 public class OrderItemStatusData {
-<<<<<<< HEAD
+
     public OrderItemStatus changeStatus (String status){
         OrderItemStatus orderItemStatus = new OrderItemStatus();
         orderItemStatus.setStatus(status);
         return  orderItemStatus;
     }
-=======
->>>>>>> Maks
+
 }
