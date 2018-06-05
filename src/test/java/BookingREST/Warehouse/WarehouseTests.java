a91cb84811a2991ab0e20422edcdf33b2c5a81f6
Volodymyr Holovko <volodymyr.holovko@magnise.com>
2018-06-05 12:31:12 +0300

    Warehouse

---
+++ src/test/java/BookingREST/Warehouse/WarehouseTests.java
@@ -0,0 +1,8 @@
+package BookingREST.Warehouse;
+
+public class WarehouseTests {
+    String token;
+    String baseURL = "http://213.136.86.27:8086/api/v1.0/warehouses/";
+    public int id;
+    WarehouseData warehouseData = new WarehouseData();
+}
