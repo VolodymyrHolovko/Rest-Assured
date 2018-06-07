f24418e236fb86fff8ed5f629e8b11bbffaaa4a3
Max <lutkovec@gmail.com>
2018-06-07 12:06:02 +0300

    no message

--- src/test/java/BookingREST/Bookings/BookingsTests.java
+++ src/test/java/BookingREST/Bookings/BookingsTests.java
@@ -27,6 +27,7 @@ public class BookingsTests {
     int serviceId;
     String usertoken;
     String token;
+    String customerId;
     String userId = "f4423ba3-545b-4cee-ad63-c030f1dbf8e3";
 
 
@@ -41,6 +42,7 @@ public class BookingsTests {
 
         GetUserToken getUserToken= new GetUserToken();
         this.usertoken = getUserToken.GetUserToken();
+        this.customerId= getUserToken.GetUserId();
 
         this.token = createBusiness.B_returncreatedToken();
     }
@@ -163,6 +165,112 @@ public class BookingsTests {
         Assert.assertEquals(true,response.asString().contains("2018-06-25 14:00:00"));
         Assert.assertEquals(true,response.asString().contains("2018-06-25 13:45:00"));
         Assert.assertEquals(true,response.asString().contains("2018-06-25 14:15:00"));
+    }
+
+    @Test
+    public void H_createCustomerBooking() {
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", usertoken)
+                .body(bookingsData.createBooking(businesId,adressId,staffId,serviceId,userId))
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().post(baseUrl).thenReturn().body();
+        BookingsResponse bookingsResponse= new Gson().fromJson(response.asString(), BookingsResponse.class);
+        Booking booking = bookingsResponse.data;
+        this.bookingId = booking.getId();
+        Assert.assertEquals(businesId,booking.getBusiness_id());
+        Assert.assertEquals(adressId,booking.getAddress_id());
+        Assert.assertEquals(staffId,booking.getStaff_id());
+        Assert.assertEquals(serviceId,booking.getService_id());
+        Assert.assertEquals(20,booking.getDuration());
+        Assert.assertEquals(700,booking.getAmount());
+        Assert.assertEquals("6559fafa-0b89-4779-b999-3ab961da149a",booking.getCustomer_id());
+        Assert.assertEquals(2,booking.getStatus());
+        Assert.assertEquals(1,booking.getType());
+        Assert.assertEquals("2018-06-25 17:00:00",booking.getDate());
+    }
+
+    @Test
+    public void I_approveCustomerBooking(){
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().patch(baseUrl+bookingId+"/").thenReturn().body();
+    }
+
+    @Test
+    public void J_getLockedTimeSlotsBooking(){
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().get("http://213.136.86.27:8085/api/v1.0/businesses/"+businesId+"/time-slots/?address_id="+adressId+"&service_id="+serviceId+"&staff_id="+staffId+"&date=2018-06-25").thenReturn().body();
+        BookingListResponse bookingListResponse= new Gson().fromJson(response.asString(), BookingListResponse.class);
+        Assert.assertEquals(false,response.asString().contains("2018-06-25 17:00:00"));
+        Assert.assertEquals(false,response.asString().contains("2018-06-25 16:45:00"));
+        Assert.assertEquals(false,response.asString().contains("2018-06-25 17:15:00"));
+    }
+
+    @Test
+    public void K_cancelBooking(){
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", usertoken)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().patch(baseUrl+bookingId+"/cancel").thenReturn().body();
+        BookingsResponse bookingsResponse= new Gson().fromJson(response.asString(), BookingsResponse.class);
+        Booking booking = bookingsResponse.data;
+        Assert.assertEquals(businesId,booking.getBusiness_id());
+        Assert.assertEquals(adressId,booking.getAddress_id());
+        Assert.assertEquals(staffId,booking.getStaff_id());
+        Assert.assertEquals(serviceId,booking.getService_id());
+        Assert.assertEquals(20,booking.getDuration());
+        Assert.assertEquals(700,booking.getAmount());
+        Assert.assertEquals("6559fafa-0b89-4779-b999-3ab961da149a",booking.getCustomer_id());
+        Assert.assertEquals(0,booking.getStatus());
+        Assert.assertEquals(1,booking.getType());
+        Assert.assertEquals("2018-06-25 17:00:00",booking.getDate());
+    }
+
+    @Test
+    public void L_getFreeTimeSlotsBooking(){
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().get("http://213.136.86.27:8085/api/v1.0/businesses/"+businesId+"/time-slots/?address_id="+adressId+"&service_id="+serviceId+"&staff_id="+staffId+"&date=2018-06-25").thenReturn().body();
+        BookingListResponse bookingListResponse= new Gson().fromJson(response.asString(), BookingListResponse.class);
+        Assert.assertEquals(true,response.asString().contains("2018-06-25 14:00:00"));
+        Assert.assertEquals(true,response.asString().contains("2018-06-25 13:45:00"));
+        Assert.assertEquals(true,response.asString().contains("2018-06-25 14:15:00"));
+    }
+
+    @Test
+    public void M_getCustomerBooking(){
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().get(baseUrl+bookingId+"/").thenReturn().body();
+        BookingsResponse bookingsResponse= new Gson().fromJson(response.asString(), BookingsResponse.class);
+        Booking booking = bookingsResponse.data;
+        Assert.assertEquals(businesId,booking.getBusiness_id());
+        Assert.assertEquals(adressId,booking.getAddress_id());
+        Assert.assertEquals(staffId,booking.getStaff_id());
+        Assert.assertEquals(serviceId,booking.getService_id());
+        Assert.assertEquals(20,booking.getDuration());
+        Assert.assertEquals(700,booking.getAmount());
+        Assert.assertEquals("6559fafa-0b89-4779-b999-3ab961da149a",booking.getCustomer_id());
+        Assert.assertEquals(0,booking.getStatus());
+        Assert.assertEquals(1,booking.getType());
+        Assert.assertEquals("2018-06-25 17:00:00",booking.getDate());
     }
 
     @AfterClass
