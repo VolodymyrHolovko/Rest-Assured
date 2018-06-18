927999b570325ce68d135172c9e51a7a0ccf1c15
Volodymyr Holovko <volodymyr.holovko@magnise.com>
2018-06-18 12:22:57 +0300

    categoryWarehouse add/update

--- src/test/java/BookingREST/СategoryWarehouses/CategoryWarehousesTests.java
+++ src/test/java/BookingREST/СategoryWarehouses/CategoryWarehousesTests.java
@@ -8,6 +8,7 @@ import com.jayway.restassured.filter.log.RequestLoggingFilter;
 import com.jayway.restassured.filter.log.ResponseLoggingFilter;
 import com.jayway.restassured.http.ContentType;
 import com.jayway.restassured.response.ResponseBody;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -17,9 +18,11 @@ public class CategoryWarehousesTests {
     int id;
     String token;
     int business_id;
+    String node_id;
     String baseURL = "http://213.136.86.27:8086/api/v1.0/categories/";
     Faker faker = new Faker();
     String name = faker.app().name().toLowerCase();
+    String name2 = faker.app().name().toLowerCase();
     CategoryWarehousesData categoryWarehousesData = new CategoryWarehousesData();
 
     @BeforeClass
@@ -42,6 +45,30 @@ public class CategoryWarehousesTests {
                 .when().post(baseURL).thenReturn().body();
         CategoryWarehousesResponse categoryWarehousesResponse = new Gson().fromJson(response.asString(), CategoryWarehousesResponse.class);
         CategoryWarehouses addCategory = categoryWarehousesResponse.data;
-        System.out.println(response.asString());
+        this.id = addCategory.getId();
+        this.node_id = String.valueOf(addCategory.getId());
+        Assert.assertEquals(id, addCategory.getId());
+        Assert.assertEquals(business_id, addCategory.getBusiness_id());
+        Assert.assertEquals(null, addCategory.getNode_id());
+        Assert.assertEquals(name, addCategory.getName());
+    }
+    @Test
+    public void B_updateCategory() {
+        CategoryWarehouses updateCategories = categoryWarehousesData.updateCategory(node_id, name2);
+        ResponseBody response = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", token)
+                .body(updateCategories)
+                .filter(new RequestLoggingFilter())
+                .filter(new ResponseLoggingFilter())
+                .when().put(baseURL + id + "/").thenReturn().body();
+        CategoryWarehousesResponse categoryWarehousesResponse = new Gson().fromJson(response.asString(), CategoryWarehousesResponse.class);
+        CategoryWarehouses updateCategory = categoryWarehousesResponse.data;
+        Assert.assertEquals(id, updateCategory.getId());
+        Assert.assertEquals(node_id, updateCategory.getNode_id());
+        Assert.assertEquals(name2, updateCategory.getName());
+        Assert.assertEquals(1, updateCategory.getNested_level());
+        Assert.assertEquals(true, updateCategory.getUpdated_at().contains("2018"));
+
     }
 }
