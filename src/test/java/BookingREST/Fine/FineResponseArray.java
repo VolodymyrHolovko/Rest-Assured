923a24406b4489d4aa06b37abae5e8b08968e61c
Max <lutkovec@gmail.com>
2018-07-06 16:59:06 +0300

    no message

---
+++ src/test/java/BookingREST/Fine/FineResponseArray.java
@@ -0,0 +1,15 @@
+package BookingREST.Fine;
+
+import java.util.List;
+
+public class FineResponseArray {
+    List<Fine> data;
+
+    public List<Fine> getData() {
+        return data;
+    }
+
+    public void setData(List<Fine> data) {
+        this.data = data;
+    }
+}
