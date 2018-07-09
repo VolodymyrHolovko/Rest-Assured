e003e22f108fd7822dfa3d145ff8895c68919c58
Max <lutkovec@gmail.com>
2018-07-09 10:32:42 +0300

    no message

--- src/test/java/BookingREST/Products/ProductsData.java
+++ src/test/java/BookingREST/Products/ProductsData.java
@@ -9,12 +9,6 @@ public class ProductsData {
         addProducts.setCategory_id(category_id);
         addProducts.setSale_used(true);
         addProducts.setUnit_id(sale_unit_id);
-        addProducts.setSale_currency(sale_currency);
-        addProducts.setSale_cost(sale_cost);
-        addProducts.setExpense_used(true);
-        addProducts.setExpense_unit_id(expense_unit_id);
-        addProducts.setExpense_currency(expense_currency);
-        addProducts.setExpense_cost(expense_cost);
         return addProducts;
     }
     public Products updateProducts(String name2, String sku2, int category_id2, int sale_unit_id2, String sale_currency2, int sale_cost2, int expense_unit_id2, String expense_currency2, int expense_cost2) {
@@ -24,12 +18,6 @@ public class ProductsData {
         updateProduct.setCategory_id(category_id2);
         updateProduct.setSale_used(false);
         updateProduct.setUnit_id(sale_unit_id2);
-        updateProduct.setSale_currency(sale_currency2);
-        updateProduct.setSale_cost(sale_cost2);
-        updateProduct.setExpense_used(false);
-        updateProduct.setExpense_unit_id(expense_unit_id2);
-        updateProduct.setExpense_currency(expense_currency2);
-        updateProduct.setExpense_cost(expense_cost2);
         return updateProduct;
     }
 
