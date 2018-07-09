860bd50433eef32e32221a72feca068426307f63
Max <lutkovec@gmail.com>
2018-07-09 10:34:53 +0300

    no message

--- src/test/java/BookingREST/Products/Products.java
+++ src/test/java/BookingREST/Products/Products.java
@@ -10,12 +10,7 @@ public class Products {
     @JsonProperty
     private boolean sale_used;
     private int unit_id;
-    private String sale_currency;
-    private int sale_cost;
-    private boolean expense_used;
-    private int expense_unit_id;
-    private String expense_currency;
-    private int expense_cost;
+
     private int id;
     private String updated_at;
     private String deleted_at;
@@ -68,53 +63,7 @@ public class Products {
         this.unit_id = unit_id;
     }
 
-    public String getSale_currency() {
-        return sale_currency;
-    }
 
-    public void setSale_currency(String sale_currency) {
-        this.sale_currency = sale_currency;
-    }
-
-    public int getSale_cost() {
-        return sale_cost;
-    }
-
-    public void setSale_cost(int sale_cost) {
-        this.sale_cost = sale_cost;
-    }
-
-    public boolean isExpense_used() {
-        return expense_used;
-    }
-
-    public void setExpense_used(boolean expense_used) {
-        this.expense_used = expense_used;
-    }
-
-    public int getExpense_unit_id() {
-        return expense_unit_id;
-    }
-
-    public void setExpense_unit_id(int expense_unit_id) {
-        this.expense_unit_id = expense_unit_id;
-    }
-
-    public String getExpense_currency() {
-        return expense_currency;
-    }
-
-    public void setExpense_currency(String expense_currency) {
-        this.expense_currency = expense_currency;
-    }
-
-    public int getExpense_cost() {
-        return expense_cost;
-    }
-
-    public void setExpense_cost(int expense_cost) {
-        this.expense_cost = expense_cost;
-    }
 
     public int getId() {
         return id;
